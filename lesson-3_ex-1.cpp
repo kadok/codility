97128622414bb36f8127517ac4478c3180ced4da
kadok <rmoraes@inf.puc-rio.br>
2018-10-21 12:35:48 -0300

    Create lesson-3_ex-1.cpp
    Optimized version.

---
+++ lesson-3_ex-1.cpp
@@ -0,0 +1,19 @@
+// you can use includes, for example:
+// #include <algorithm>
+#include <numeric>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    int result = 0;
+ 
+    for (int i = 0; i < A.size(); i++)
+    {
+        result += (i + 1 - A[i]);
+    }
+     
+    result += int(A.size() + 1);     
+    return result;
+}
