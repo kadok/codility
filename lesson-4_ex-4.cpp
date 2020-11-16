eb01fe5ab08df9758894cdb203fcd5a3d71c9ef0
kadok <renatomoraesdossantos@gmail.com>
2020-11-16 18:45:28 -0300

    Adding Lesson 4 - exercise 4 100%

--- lesson-4_ex-4.cpp
+++ lesson-4_ex-4.cpp
@@ -1,38 +1,35 @@
-/*This is a demo task.
-
-Write a function:
-
-int solution(vector<int> &A);
-
-that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
-
-For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
-
-Given A = [1, 2, 3], the function should return 4.
-
-Given A = [−1, −3], the function should return 1.
-
-Write an efficient algorithm for the following assumptions:
-
-N is an integer within the range [1..100,000];
-each element of array A is an integer within the range [−1,000,000..1,000,000].*/
-
 // you can use includes, for example:
 #include <algorithm>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
 
 // you can write to stdout for debugging purposes, e.g.
 // cout << "this is a debug message" << endl;
 
 int solution(vector<int> &A)
 {
-    std::sort (A.begin(), A.end());
-    int missing = 1;
-    for (int i = 0; i < A.size(); i++)
+    // write your code in C++14 (g++ 6.2.0)
+    unsigned int N = A.size();
+    sort(A.begin(), A.end()); 
+    
+    map<int, int> m;
+
+    for (unsigned int i = 0; i < N; i++)
     {
-        if (A[i] == missing)
-            missing +=1;
-        if (A[i] > missing)
-            break;
+        if (A[i] != i+1)
+            return 0;
+        
+        if (m.insert(pair<int,int>(i,A[i])).second == false)
+            return 0;
     }
-    return missing;
+    
+    if (m.size() == A[N-1])
+    {
+        return 1;
+    }
+    
+    return 0;
 }
